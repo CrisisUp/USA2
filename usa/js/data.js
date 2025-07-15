3a54f1adaba1436c79c6ebd84ac6c998b16fdd3b
CrisisUp <cristiano.1973.on@gmail.com>
2025-07-14 22:15:26 -0300

    commit de mais imagens 2

--- usa/js/data.js
+++ usa/js/data.js
@@ -54,9 +54,9 @@ export const stateData = {
 
     "MT": { name: "Montana", media: [
 
-        { type: "Série", title: "Yellowstone", description: "A saga da família Dutton, proprietários da maior fazenda dos EUA, lutando para manter suas terras.", cover: "https://image.tmdb.org/t/p/w500/s3bQY9y4d3g6h7i8j9k0l1m2n3p.jpg", rating: "8.7/10", imdbLink: "https://www.imdb.com/title/tt4236770/" }, 
+        { type: "Série", title: "Yellowstone", description: "A saga da família Dutton, proprietários da maior fazenda dos EUA, lutando para manter suas terras.", cover: "images/Montana/yellowstone.png", rating: "8.7/10", imdbLink: "https://www.imdb.com/title/tt4236770/" }, 
 
-        { type: "Filme", title: "Nada é por acaso", description: "Um homem tenta reconstruir sua vida em Montana após um evento trágico.", cover: "https://image.tmdb.org/t/p/w500/q1w2e3r4t5y6u7i8o9p0q1r2s3t.jpg", rating: "6.5/10", imdbLink: "https://www.imdb.com/title/tt0437435/" }] },
+        { type: "Série", title: "1883", description: "Prequela de Yellowstone que mostra a origem da família Dutton na expansão para Montana.", cover: "images/Montana/1883.png", rating: "6.5/10", imdbLink: "https://www.imdb.com/title/tt13991232/" }] },
 
     "ND": { name: "Dakota do Norte", media: [
 
